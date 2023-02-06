ae2afb2dbd1ce63252a9e07472eb47a3a5ee44b1
Henderson <henderson.porfirio@spassu.com.br>
2023-02-06 10:45:50 -0300

    # commit da batch

--- src/main/java/com/springbatch/enviopromocoesemail/config/DataSourceConfig.java
+++ src/main/java/com/springbatch/enviopromocoesemail/config/DataSourceConfig.java
@@ -1,12 +1,12 @@
 package com.springbatch.enviopromocoesemail.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
